1b652da0c02bfb09c0d74a7ac0624b5b3c6bd929
Pontus Abrahamsson <pontus@lostisland.co>
2017-04-29 18:23:34 +0200

    autoupdate

--- src/update.js
+++ src/update.js
@@ -1,35 +1,41 @@
-const log = require('electron-log');
 const { autoUpdater } = require('electron-updater');
+const logger = require('electron-log');
+const ms = require('ms');
+
+autoUpdater.logger = logger;
+autoUpdater.logger.transports.file.level = 'info';
+
+// Check for updates every 5 minute
+const createInterval = () => setInterval(() => {
+  logger.info('Checking for updates');
+  autoUpdater.checkForUpdates();
+}, ms('5m'));
 
 function update() {
-  autoUpdater.logger = log;
-  autoUpdater.logger.transports.file.level = 'info';
-  log.info('App starting...');
+  // at this point the app is fully started and ready for updating
+  setTimeout(() => autoUpdater.checkForUpdates(), ms('5s'));
 
-  autoUpdater.checkForUpdates();
-
-  autoUpdater.on('checking-for-update', () => {
-    console.log('Checking for update...');
-  });
+  let intervalId = createInterval();
 
   autoUpdater.on('update-available', () => {
-    console.log('Update available.');
+    logger.info('update-available');
+    clearInterval(intervalId);
+    intervalId = null;
   });
 
-  autoUpdater.on('update-not-available', () => {
-    console.log('Update not available.');
-  });
-
-  autoUpdater.on('error', () => {
-    console.log('Error in auto-updater.');
-  });
-
-  autoUpdater.on('download-progress', () => {
-    console.log('Download progress...');
-  });
 
   autoUpdater.on('update-downloaded', () => {
-    console.log('Update downloaded; will install in 5 seconds');
+    logger.info('update-downloaded');
+  });
+
+  autoUpdater.on('update-not-available', () => logger.info('update-not-available'));
+
+
+  autoUpdater.on('error', (err) => {
+    if (intervalId === null) {
+      intervalId = createInterval();
+    }
+    logger.info('Error fetching updates', err.stack);
   });
 }
 
